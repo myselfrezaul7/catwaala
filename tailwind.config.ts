8334b83b490d83976131661124709f1e4ed96d17
shawono7 <myselfrezaul@outlook.com>
2026-01-27 02:21:28 +0100

    Switch to IBM Plex Sans font

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,149 +1,149 @@
 import type { Config } from "tailwindcss"
 
 const config = {
-    darkMode: ["class"],
-    content: [
-        './pages/**/*.{ts,tsx}',
-        './components/**/*.{ts,tsx}',
-        './app/**/*.{ts,tsx}',
-        './src/**/*.{ts,tsx}',
-    ],
-    prefix: "",
-    theme: {
-    	container: {
-    		center: true,
-    		padding: '2rem',
-    		screens: {
-    			'2xl': '1400px'
-    		}
-    	},
-    	extend: {
-    		fontFamily: {
-    			sans: [
-    				'var(--font-poppins)',
-    				'sans-serif'
-    			],
-    			heading: [
-    				'var(--font-poppins)',
-    				'sans-serif'
-    			]
-    		},
-    		colors: {
-    			border: 'hsl(var(--border))',
-    			input: 'hsl(var(--input))',
-    			ring: 'hsl(var(--ring))',
-    			background: 'hsl(var(--background))',
-    			foreground: 'hsl(var(--foreground))',
-    			primary: {
-    				DEFAULT: 'hsl(var(--primary))',
-    				foreground: 'hsl(var(--primary-foreground))'
-    			},
-    			secondary: {
-    				DEFAULT: 'hsl(var(--secondary))',
-    				foreground: 'hsl(var(--secondary-foreground))'
-    			},
-    			destructive: {
-    				DEFAULT: 'hsl(var(--destructive))',
-    				foreground: 'hsl(var(--destructive-foreground))'
-    			},
-    			muted: {
-    				DEFAULT: 'hsl(var(--muted))',
-    				foreground: 'hsl(var(--muted-foreground))'
-    			},
-    			accent: {
-    				DEFAULT: 'hsl(var(--accent))',
-    				foreground: 'hsl(var(--accent-foreground))'
-    			},
-    			popover: {
-    				DEFAULT: 'hsl(var(--popover))',
-    				foreground: 'hsl(var(--popover-foreground))'
-    			},
-    			card: {
-    				DEFAULT: 'hsl(var(--card))',
-    				foreground: 'hsl(var(--card-foreground))'
-    			},
-    			rose: {
-    				'50': '#fff1f2',
-    				'100': '#ffe4e6',
-    				'200': '#fecdd3',
-    				'300': '#fda4af',
-    				'400': '#fb7185',
-    				'500': '#f43f5e',
-    				'600': '#e11d48',
-    				'700': '#be123c',
-    				'800': '#9f1239',
-    				'900': '#881337',
-    				'950': '#4c0519'
-    			},
-    			indigo: {
-    				'50': '#eef2ff',
-    				'100': '#e0e7ff',
-    				'200': '#c7d2fe',
-    				'300': '#a5b4fc',
-    				'400': '#818cf8',
-    				'500': '#6366f1',
-    				'600': '#4f46e5',
-    				'700': '#4338ca',
-    				'800': '#3730a3',
-    				'900': '#312e81',
-    				'950': '#1e1b4b'
-    			},
-    			chart: {
-    				'1': 'hsl(var(--chart-1))',
-    				'2': 'hsl(var(--chart-2))',
-    				'3': 'hsl(var(--chart-3))',
-    				'4': 'hsl(var(--chart-4))',
-    				'5': 'hsl(var(--chart-5))'
-    			}
-    		},
-    		borderRadius: {
-    			lg: 'var(--radius)',
-    			md: 'calc(var(--radius) - 2px)',
-    			sm: 'calc(var(--radius) - 4px)'
-    		},
-    		keyframes: {
-    			'accordion-down': {
-    				from: {
-    					height: '0'
-    				},
-    				to: {
-    					height: 'var(--radix-accordion-content-height)'
-    				}
-    			},
-    			'accordion-up': {
-    				from: {
-    					height: 'var(--radix-accordion-content-height)'
-    				},
-    				to: {
-    					height: '0'
-    				}
-    			},
-    			float: {
-    				'0%, 100%': {
-    					transform: 'translateY(0)'
-    				},
-    				'50%': {
-    					transform: 'translateY(-20px)'
-    				}
-    			},
-    			'pulse-slow': {
-    				'0%, 100%': {
-    					opacity: '1'
-    				},
-    				'50%': {
-    					opacity: '0.6'
-    				}
-    			}
-    		},
-    		animation: {
-    			'accordion-down': 'accordion-down 0.2s ease-out',
-    			'accordion-up': 'accordion-up 0.2s ease-out',
-    			float: 'float 6s ease-in-out infinite',
-    			'pulse-slow': 'pulse-slow 4s ease-in-out infinite'
-    		}
-    	}
-    },
-    plugins: [require("tailwindcss-animate")],
+	darkMode: ["class"],
+	content: [
+		'./pages/**/*.{ts,tsx}',
+		'./components/**/*.{ts,tsx}',
+		'./app/**/*.{ts,tsx}',
+		'./src/**/*.{ts,tsx}',
+	],
+	prefix: "",
+	theme: {
+		container: {
+			center: true,
+			padding: '2rem',
+			screens: {
+				'2xl': '1400px'
+			}
+		},
+		extend: {
+			fontFamily: {
+				sans: [
+					'var(--font-ibm-plex-sans)',
+					'sans-serif'
+				],
+				heading: [
+					'var(--font-ibm-plex-sans)',
+					'sans-serif'
+				]
+			},
+			colors: {
+				border: 'hsl(var(--border))',
+				input: 'hsl(var(--input))',
+				ring: 'hsl(var(--ring))',
+				background: 'hsl(var(--background))',
+				foreground: 'hsl(var(--foreground))',
+				primary: {
+					DEFAULT: 'hsl(var(--primary))',
+					foreground: 'hsl(var(--primary-foreground))'
+				},
+				secondary: {
+					DEFAULT: 'hsl(var(--secondary))',
+					foreground: 'hsl(var(--secondary-foreground))'
+				},
+				destructive: {
+					DEFAULT: 'hsl(var(--destructive))',
+					foreground: 'hsl(var(--destructive-foreground))'
+				},
+				muted: {
+					DEFAULT: 'hsl(var(--muted))',
+					foreground: 'hsl(var(--muted-foreground))'
+				},
+				accent: {
+					DEFAULT: 'hsl(var(--accent))',
+					foreground: 'hsl(var(--accent-foreground))'
+				},
+				popover: {
+					DEFAULT: 'hsl(var(--popover))',
+					foreground: 'hsl(var(--popover-foreground))'
+				},
+				card: {
+					DEFAULT: 'hsl(var(--card))',
+					foreground: 'hsl(var(--card-foreground))'
+				},
+				rose: {
+					'50': '#fff1f2',
+					'100': '#ffe4e6',
+					'200': '#fecdd3',
+					'300': '#fda4af',
+					'400': '#fb7185',
+					'500': '#f43f5e',
+					'600': '#e11d48',
+					'700': '#be123c',
+					'800': '#9f1239',
+					'900': '#881337',
+					'950': '#4c0519'
+				},
+				indigo: {
+					'50': '#eef2ff',
+					'100': '#e0e7ff',
+					'200': '#c7d2fe',
+					'300': '#a5b4fc',
+					'400': '#818cf8',
+					'500': '#6366f1',
+					'600': '#4f46e5',
+					'700': '#4338ca',
+					'800': '#3730a3',
+					'900': '#312e81',
+					'950': '#1e1b4b'
+				},
+				chart: {
+					'1': 'hsl(var(--chart-1))',
+					'2': 'hsl(var(--chart-2))',
+					'3': 'hsl(var(--chart-3))',
+					'4': 'hsl(var(--chart-4))',
+					'5': 'hsl(var(--chart-5))'
+				}
+			},
+			borderRadius: {
+				lg: 'var(--radius)',
+				md: 'calc(var(--radius) - 2px)',
+				sm: 'calc(var(--radius) - 4px)'
+			},
+			keyframes: {
+				'accordion-down': {
+					from: {
+						height: '0'
+					},
+					to: {
+						height: 'var(--radix-accordion-content-height)'
+					}
+				},
+				'accordion-up': {
+					from: {
+						height: 'var(--radix-accordion-content-height)'
+					},
+					to: {
+						height: '0'
+					}
+				},
+				float: {
+					'0%, 100%': {
+						transform: 'translateY(0)'
+					},
+					'50%': {
+						transform: 'translateY(-20px)'
+					}
+				},
+				'pulse-slow': {
+					'0%, 100%': {
+						opacity: '1'
+					},
+					'50%': {
+						opacity: '0.6'
+					}
+				}
+			},
+			animation: {
+				'accordion-down': 'accordion-down 0.2s ease-out',
+				'accordion-up': 'accordion-up 0.2s ease-out',
+				float: 'float 6s ease-in-out infinite',
+				'pulse-slow': 'pulse-slow 4s ease-in-out infinite'
+			}
+		}
+	},
+	plugins: [require("tailwindcss-animate")],
 } satisfies Config
 
 export default config
